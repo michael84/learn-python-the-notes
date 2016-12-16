c96e50ad8883350db21398c87d566b4fcfa40747
Wei Tong <michaeltong84@163.com>
2016-12-16 15:57:17 +0800

    adding ex27, ex28

---
+++ learnpythonthehardway/ex27-28.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import ex_name
+
+ex_name.title("ex27: Memorizing Logic")
+
+#Codes in the book (some words may be changed):
+#
+print """ The Truth Terms:
+* and
+* or
+* not
+* != (not equal)
+* == (equal)
+* >= (greater-than-equal)
+* <= (less-than-equal)
+* True
+* False
+"""
+
+print """
+
+"""
+
+
+
+#####################################
+ex_name.title("ex28: Boolean Practice")
