eddd7d3101ef5bd162e7357f9c16a2f2ffb36127
Wei Tong <michaeltong84@163.com>
2016-12-20 14:08:43 +0800

    Ex37: Symbol Review

---
+++ learnpythonthehardway/ex37.py
@@ -0,0 +1,116 @@
+
+# Keywords
+print """Keywords:
+* and - Logical and.    Example: True and False == False
+* as  - Part of the with-as statement.      Example: with X as Y: pass
+* assert - Assert (ensure) that something is true.      Example: assert False, "Error!"
+* break - Stop this loop right now. Example: while True: break
+* class - Define a class. Example: class Person(object)
+* continue - Don't process more of the loop, do it again. Example: while True: continue
+* def - Define a function. Example: def X(): pass
+* del - Delete from dictionary. Example: del X[Y]
+* elif - Else if condition. 
+* else - Else condition.
+* except - If an exception happens, do this. Example: except ValueError, e: print e
+* exec - Run a string as python. Example: exec 'print "hello"'
+* finally - Exceptions or not, finally do this no matter what. Example: finally: pass
+* for - Loop over a collection of things. Example: for X in Y: pass
+* from - Importing specific parts of a module. Example: from X import Y
+* global - Declare that you want a global variable. Example: global X
+* if - If condition.
+* import - import a module into this one to use.
+* in - Part of for-loops. Also a test of X in Y. Example: for X in Y: pass| 1 in [1] == True
+* is - Like == to test equality. Example: 1 is 1 ==True
+* lambda - Create a short anonymous function. Example: s = lambda y: y**y; s(3)
+* not - Logical not. Example: not True == False
+* or - Logic or. Example: True or False == True
+* pass - This block is empty. Example: def empty(): pass
+* print - Print this string. Example: print 'This string'
+* raise - Raise an exception when things go wrong. Example: raise ValueError("No")
+* return - Exit the function with a return value. Example: def X(): return Y
+* try - Try this block, and if exception, go to except. Example: try: pass
+* while - While loop. Example: while X: pass
+* with - With an expression as a variable do. Example: with X as Y: pass
+* yield - Pause here and return to caller. Example: def X(): yield Y; X().next()
+"""
+
+##
+print """Data Types:
+* True
+* False
+* None
+* strings
+* numbers
+* floats
+* lists
+* dicts
+"""
+
+##
+print """Escape Sequences:
+* \\\\  - Backslash
+* \\'   - Single-quote
+* \\"   - Double-quote
+* \\a   - Bell
+* \\b   - Backspace
+* \\f   - Formfeed
+* \\n   - Newline
+* \\r   - Carriage
+* \\t   - Tab
+* \\v   - Vertical tab
+"""
+
+##
+print """String Formats
+* %d
+* %i
+* %o
+* %u
+* %x
+* %X
+* %e
+* %E
+* %f
+* %F
+* %g
+* %G
+* %c
+* %r
+* %s
+* %%
+
+"""
+
+##
+print """
+* +
+* -
+* *
+* **
+* /
+* //
+* %
+* <
+* >
+* <=
+* >=
+* ==
+* !=
+* <>
+* ()
+* []
+* {}
+* @
+* ,
+* :
+* .
+* =
+* ;
+* +=
+* -=
+* *=
+* /=
+* //=
+* %=
+* **=
+"""
